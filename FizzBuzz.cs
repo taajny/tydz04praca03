aba701822409c050b93142da8cf102963026a8e4
Krzysztof <taajny@gmail.com>
2023-08-31 22:06:15 +0200

    Dodaj pliki projektów.

---
+++ FizzBuzz.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tydz04praca02
+{
+    internal class FizzBuzz
+    {
+        public string Play(int number)
+        {
+            if (number % 5 == 0 && number % 3 == 0)
+                return "FizzBuzz";
+            else if (number % 5 == 0)
+                return "Buzz";
+            else if (number % 3 == 0)
+                return "Fizz";
+            else
+                return number.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tydz04praca02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var fizzBuzzObject = new FizzBuzz();
+            int number = 0;
+            string choise = "";
+            char character;
+
+            Console.WriteLine("Witaj w grze Fizz Buzz. Zaczynamy");
+            
+            while (true)
+            {
+                number = GetNumber();
+                Console.WriteLine($"\nWynik naszej gry to: {fizzBuzzObject.Play(number)}");
+
+                while (true)
+                {
+                    
+                    Console.WriteLine("\nCzy chcesz zagrać jeszcze raz [T/N]");
+                    if (string.IsNullOrWhiteSpace(choise = Console.ReadLine()))
+                    {
+                        Console.WriteLine("\nPodałeś niepoprawne dane.");
+                        continue;
+                    }
+                    choise = choise.ToUpper();
+                    character = (char)choise[0];
+
+                    if (character != 'T' && character != 'N')
+                    {
+                        Console.WriteLine("\nPodałeś niepoprawne dane.");
+                        continue;
+                    }
+                    else
+                        break;
+                }
+                if (character == 'N')
+                    break;
+            }
+            Console.ReadLine();
+        }
+        private static int GetNumber()
+        {
+            while (true)
+            {
+                Console.WriteLine("\nPodaj proszę liczbę: ");
+                if (!int.TryParse(Console.ReadLine(), out var number))
+                {
+                    Console.WriteLine("\nPodałeś niepoprawne dane. Podaj proszę liczbę.");
+                    continue;
+                }
+                else
+                    return number;
+            }
+            
+        }
+    }
+}
